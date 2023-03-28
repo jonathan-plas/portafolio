00a4d28d7d41c31300d50efe57ec3f98cb575ea4
Jonathan <Jonathan.plasencia20@gmail.com>
2023-03-28 15:54:52 -0500

    Ultimos cambios

--- Js/animaciones.js
+++
@@ -1,9 +0,0 @@
-const menu = document.querySelector('#menu');
-
-menu.addEventListener('click', (event) => {
-  if (event.target.tagName === 'A') {
-    event.preventDefault();
-    const section = document.querySelector(event.target.hash);
-    section.scrollIntoView({ behavior: 'smooth' });
-  }
-});
